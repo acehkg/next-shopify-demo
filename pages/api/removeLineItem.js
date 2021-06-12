919578e6ca49d00afa2970ca29ca910204454c05
Andrew <andrewembury@me.com>
2021-06-12 04:24:56 -0700

    some basic cleanup

--- pages/api/removeLineItem.js
+++ pages/api/removeLineItem.js
@@ -4,11 +4,6 @@ export default async function handler(req, res) {
   // remove line item
   const { variantId, checkoutId } = JSON.parse(req.body);
   const itemToRemove = [variantId];
-
-  try {
-    await shopifyClient.checkout.removeLineItems(checkoutId, itemToRemove);
-    res.status(200).json();
-  } catch (e) {
-    console.log(e);
-  }
+  await shopifyClient.checkout.removeLineItems(checkoutId, itemToRemove);
+  res.status(200).json();
 }
