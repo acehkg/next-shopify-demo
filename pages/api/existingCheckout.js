2cfde982be5ccce56ffc052861455d813eee0f79
Andrew <andrewembury@me.com>
2021-06-13 16:38:14 -0800

    implemented hacky solution

--- pages/api/existingCheckout.js
+++ pages/api/existingCheckout.js
@@ -5,5 +5,7 @@ export default async function handler(req, res) {
   try {
     const checkout = await shopifyClient.checkout.fetch(req.body);
     res.status(200).json(checkout);
-  } catch (err) {}
+  } catch (err) {
+    return res.status(200).json({ msg: 'Waiting For Checkout ID' });
+  }
 }
