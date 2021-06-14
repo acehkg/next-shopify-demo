029d70508e665b29c70a63e4053334a1998f9c80
Andrew <andrewembury@me.com>
2021-06-13 17:07:00 -0800

    try again

--- pages/api/existingCheckout.js
+++ pages/api/existingCheckout.js
@@ -2,6 +2,11 @@ import { shopifyClient } from '../../utils/client';
 
 export default async function handler(req, res) {
   // fetch existing  checkout
-  const checkout = await shopifyClient.checkout.fetch(req.body);
-  res.status(200).json(checkout);
+  try {
+    const checkout = await shopifyClient.checkout.fetch(req.body);
+    res.status(200).json(checkout);
+  } catch (err) {
+    const error = JSON.stringify(err);
+    res.status(200).json(error);
+  }
 }
