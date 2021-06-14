97cb2ff84b66d3de55a43a3ded640b74150c423e
Andrew <andrewembury@me.com>
2021-06-13 16:46:11 -0800

    didnt work

--- pages/api/existingCheckout.js
+++ pages/api/existingCheckout.js
@@ -2,10 +2,6 @@ import { shopifyClient } from '../../utils/client';
 
 export default async function handler(req, res) {
   // fetch existing  checkout
-  try {
-    const checkout = await shopifyClient.checkout.fetch(req.body);
-    res.status(200).json(checkout);
-  } catch (err) {
-    return res.status(200).json({ msg: 'Waiting For Checkout ID' });
-  }
+  const checkout = await shopifyClient.checkout.fetch(req.body);
+  res.status(200).json(checkout);
 }
