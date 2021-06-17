2225f1a4427cfc9d0dfed26003844fb0cdf4ce5d
Andrew <andrewembury@me.com>
2021-06-18 07:46:37 +0800

    new shopping bag and dark mode moved

--- components/footer/PoliciesMenu.js
+++ components/footer/PoliciesMenu.js
@@ -5,15 +5,24 @@ import {
   MenuItem,
   MenuList,
   Button,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { HiPlus } from 'react-icons/hi';
 import usePolicies from '../../hooks/usePolicies';
 
 const PoliciesMenu = ({ policies, size, ...rest }) => {
+  const bg = useColorModeValue('white', 'gray.800');
   const newPolicies = usePolicies(policies);
   return (
-    <Menu isLazy placement='top'>
-      <MenuButton as={Button} rightIcon={<HiPlus size={size} />} {...rest}>
+    <Menu isLazy placement='top' {...rest}>
+      <MenuButton
+        as={Button}
+        rightIcon={<HiPlus size={size} />}
+        bg={bg}
+        _hover={{ bg: { bg } }}
+        _active={{ bg: { bg } }}
+        _focus={{ border: 'none' }}
+      >
         STORE POLICIES
       </MenuButton>
       <MenuList>
