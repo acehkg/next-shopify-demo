e0a5bc4ab8e15fbe703b82ae36e6db2c1c6b2cba
Andrew <andrewembury@me.com>
2021-06-18 14:22:13 +0800

    working on security

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+//create secure headers
+const { createSecureHeaders } = require('next-secure-headers');
+
 module.exports = {
+  async headers() {
+    return [{ source: '/(.*)', headers: createSecureHeaders() }];
+  },
+  poweredByHeader: false,
   images: {
     domains: ['cdn.shopify.com'],
   },
