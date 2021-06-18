36e02276e0567305161de0b79a28a2dec01f1266
Andrew <andrewembury@me.com>
2021-06-18 14:59:47 +0800

    create CSP headers

--- next.config.js
+++ next.config.js
@@ -3,7 +3,21 @@ const { createSecureHeaders } = require('next-secure-headers');
 
 module.exports = {
   async headers() {
-    return [{ source: '/(.*)', headers: createSecureHeaders() }];
+    return [
+      {
+        source: '/(.*)',
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: "'self' data:",
+              scriptSrc: "'self' 'unsafe-eval' 'unsafe-inline'",
+              imageSrc: "'self'  'https://*.shopify.com' ",
+              styleSrc: "'unsafe-inline'",
+            },
+          },
+        }),
+      },
+    ];
   },
   poweredByHeader: false,
   images: {
