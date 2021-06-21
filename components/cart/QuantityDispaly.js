c51bac419a06df98a7bf03c9c91d75af7e5a933f
Andrew <andrewembury@me.com>
2021-06-22 04:24:50 +0800

    added aria-label to quantity

--- components/cart/QuantityDispaly.js
+++ components/cart/QuantityDispaly.js
@@ -2,7 +2,7 @@ import { Box, useColorModeValue } from '@chakra-ui/react';
 const QuantityDisplay = ({ quantity, ...rest }) => {
   const bg = useColorModeValue('gray.100', 'gray.700');
   return (
-    <Box bg={bg} textAlign='center' {...rest}>
+    <Box bg={bg} textAlign='center' aria-label='Quantity' {...rest}>
       {quantity}
     </Box>
   );
