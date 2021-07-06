e2903369176c0bc2019be2b91aa37c3d3e8e891b
Andrew <andrewembury@me.com>
2021-07-06 12:12:00 +0800

    added absolute URL for grpahql client

--- utils/client.js
+++ utils/client.js
@@ -2,6 +2,6 @@ import Client from 'shopify-buy';
 
 // Initializing a client to return content in the store's primary language
 export const shopifyClient = Client.buildClient({
-  domain: process.env.SHOPIFY_DOMAIN,
+  domain: process.env.SHOPIFY_GRAPH_DOMAIN,
   storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_TOKEN,
 });
