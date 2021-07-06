231fb4f76b9802042b61a990ef68bda4ea32a01f
Andrew <andrewembury@me.com>
2021-07-06 12:16:04 +0800

    fixing mistakes

--- utils/graphClient.js
+++ utils/graphClient.js
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 
-const endpoint = process.env.GRAPHQL_ENDPOINT;
+const endpoint = process.env.SHOPIFY_GRAPH_DOMAIN;
 
 const storefrontClient = new GraphQLClient(endpoint, {
   headers: {
